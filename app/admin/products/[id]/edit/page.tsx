371f9393a65cacd63efa402928075f7e72038b98
Monicah Cloud <monicahcloud@gmail.com>
2025-02-25 07:37:29 -0500

    update edit product page and update image container component

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import {
+  fetchAdminProductDetails,
+  updateProductAction,
+  updateProductImageAction,
+} from "@/utils/actions";
 
-function EditProductPage() {
+import FormInput from "@/app/components/form/FormInput";
+import PriceInput from "@/app/components/form/PriceInput";
+import TextAreaInput from "@/app/components/form/TextAreaInput";
+import { SubmitButton } from "@/app/components/form/Buttons";
+import CheckboxInput from "@/app/components/form/CheckboxInput";
+import FormContainer from "@/app/components/form/FormContainer";
+import ImageInputContainer from "@/app/components/form/ImagineInputContainer";
+
+async function EditProductPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const product = await fetchAdminProductDetails(id);
+  const { name, company, description, featured, price } = product;
   return (
-    <div>EditProductPage</div>
-  )
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">update product</h1>
+      <div className="border p-8 rounded">
+        <ImageInputContainer
+          action={updateProductImageAction}
+          name={name}
+          image={product.image}
+          text="update image">
+          <input type="hidden" name="id" value={id} />
+          <input type="hidden" name="url" value={product.image} />
+        </ImageInputContainer>
+        <FormContainer action={updateProductAction}>
+          <div className="grid gap-4 md:grid-cols-2 my-4">
+            <input type="hidden" name="id" value={id} />
+            <FormInput
+              type="text"
+              name="name"
+              label="product name"
+              defaultValue={name}
+            />
+            <FormInput type="text" name="company" defaultValue={company} />
+            <PriceInput defaultValue={price} />
+          </div>
+          <TextAreaInput
+            name="description"
+            labelText="product description"
+            defaultValue={description}
+          />
+          <div className="mt-6">
+            <CheckboxInput
+              name="featured"
+              label="featured"
+              defaultChecked={featured}
+            />
+          </div>
+          <SubmitButton text="update product" className="mt-8" />
+        </FormContainer>
+      </div>
+    </section>
+  );
 }
-
-export default EditProductPage
+export default EditProductPage;
