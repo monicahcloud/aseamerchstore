0eec08dc3fe969ea65f06380f665aeea8bef3e47
Monicah Cloud <monicahcloud@gmail.com>
2025-02-24 21:42:57 -0500

    my products links is functioning

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import EmptyList from "@/app/components/global/Emptylist";
+import { deleteProductAction, fetchAdminProducts } from "@/utils/actions";
+import Link from "next/link";
 
-function AdminProductsPage() {
+import { formatCurrency } from "@/utils/format";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import FormContainer from "@/app/components/form/FormContainer";
+import { IconButton } from "@/app/components/form/Buttons";
+;
+
+async function AdminProductsPage() {
+  const items = await fetchAdminProducts();
+  if (items.length === 0) return <EmptyList />;
+
   return (
-    <div>AdminProductsPage</div>
-  )
+    <section>
+      <Table>
+        <TableCaption className="capitalize">
+          total products : {items.length}
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Product Name</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {items.map((item) => {
+            const { id: productId, name, company, price } = item;
+            return (
+              <TableRow key={productId}>
+                <TableCell>
+                  <Link
+                    href={`/products/${productId}`}
+                    className="underline text-muted-foreground tracking-wide capitalize">
+                    {name}
+                  </Link>
+                </TableCell>
+                <TableCell>{company}</TableCell>
+                <TableCell>{formatCurrency(price)}</TableCell>
+                <TableCell className="flex items-center gap-x-2">
+                  <Link href={`/admin/products/${productId}/edit`}>
+                    <IconButton actionType="edit" />
+                  </Link>
+                  <DeleteProduct productId={productId} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </section>
+  );
 }
 
-export default AdminProductsPage
+function DeleteProduct({ productId }: { productId: string }) {
+  const deleteProduct = deleteProductAction.bind(null, { productId });
+  return (
+    <FormContainer action={deleteProduct}>
+      <IconButton actionType="delete" />
+    </FormContainer>
+  );
+}
+
+export default AdminProductsPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -4,7 +4,8 @@ import { currentUser } from "@clerk/nextjs/server";
 import { prisma } from "./prisma";
 import { redirect } from "next/navigation";
 import { imageSchema, productSchema, validateWithZodSchema } from "./schemas";
-import { uploadImage } from "./supabase";
+import { deleteImage, uploadImage } from "./supabase";
+import { revalidatePath } from "next/cache";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -79,4 +80,37 @@ export const createProductAction = async (
     return renderError(error);
   }
    redirect("/admin/products");
+};
+
+const getAdminUser = async () => {
+  const user = await getAuthUser()
+  if(user.id !== process.env.ADMIN_USER_ID) redirect('/')
+    return user
+}
+
+export const fetchAdminProducts = async () => {
+  await getAdminUser()
+  const products = await prisma.product.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+  return products
+}
+
+export const deleteProductAction = async (prevState: { productId: string }) => {
+  const { productId } = prevState;
+  await getAdminUser();
+  try {
+    const product = await prisma.product.delete({
+      where: {
+        id: productId,
+      },
+    });
+    await deleteImage(product.image);
+    revalidatePath("/admin/products");
+    return { message: "product removed" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
