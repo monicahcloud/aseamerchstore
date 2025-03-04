1f8641fdc608aaf89c79c6c08393e053894d0e24
Monicah Cloud <monicahcloud@gmail.com>
2025-03-04 07:29:17 -0500

    prodcut rating component complete

--- app/components/single-product/ProductRating.tsx
+++ app/components/single-product/ProductRating.tsx
@@ -1,11 +1,12 @@
+import { fetchProductRating } from "@/utils/actions";
 import { FaStar } from "react-icons/fa";
 
 async function ProductRating({ productId }: { productId: string }) {
-  const rating = 4.2;
-  const count = 25;
-console.log(productId);
+  const { count, rating } = await fetchProductRating(productId);
+
   const className = `flex gap-1 items-center text-md mt-1 mb-4`;
   const countValue = `(${count}) reviews`;
+
   return (
     <span className={className}>
       <FaStar className="w-3 h-3" />
@@ -13,5 +14,4 @@ console.log(productId);
     </span>
   );
 }
-
 export default ProductRating;
